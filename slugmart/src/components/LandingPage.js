cf4244157c82bbb5415cc5b91e80abfa0723336c
Ethan Wong <ewong29@ucsc.edu>
2024-10-24 15:29:51 -0700

    frontend for landing page

--- slugmart/src/components/LandingPage.js
+++ slugmart/src/components/LandingPage.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './LandingPage.css';
-import { useEffect, useState } from 'react';
-import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from 'firebase/auth';
-import { auth, db } from '../config/firebase-config';
-import { doc, setDoc } from 'firebase/firestore';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import "./LandingPage.css";
+import { useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth, db } from "../config/firebase-config";
+import { doc, setDoc } from "firebase/firestore";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const [user, setUser] = useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // handling login
   const handleGoogleLogin = async () => {
@@ -21,39 +24,38 @@ function LandingPage() {
       const user = result.user;
 
       // checking if user apart of ucsc
-      if (user.email.endsWith('@ucsc.edu')){
-        console.log('User from ucsc', user);
+      if (user.email.endsWith("@ucsc.edu")) {
+        console.log("User from ucsc", user);
         addUserToDatabase(user); // add user to databse if they are ucsc user
         setUser(user);
       } else {
-        console.log('User not from ucsc');
+        console.log("User not from ucsc");
       }
     } catch (error) {
-      console.error('Error Signing in: ', error);
+      console.error("Error Signing in: ", error);
     }
-  }
+  };
 
   // adding users to database
   const addUserToDatabase = async (user) => {
     try {
-      const userRef = doc(db, 'users', user.uid);
+      const userRef = doc(db, "users", user.uid);
       await setDoc(userRef, {
         name: user.displayName,
         email: user.email,
         picture: user.photoURL,
       });
     } catch (error) {
-      console.error('Error adding user to database', error);
+      console.error("Error adding user to database", error);
     }
-  }
-
+  };
 
   useEffect(() => {
     const listener = onAuthStateChanged(auth, (user) => {
-      if (user && user.email.endsWith('@ucsc.edu')) {
-        console.log('User already logged in:', user);
+      if (user && user.email.endsWith("@ucsc.edu")) {
+        console.log("User already logged in:", user);
         setUser(user);
-        navigate('/account')
+        navigate("/account");
       } else {
         setUser(null);
       }
@@ -64,33 +66,18 @@ function LandingPage() {
   return (
     <div className="landing-container">
       <div className="flex-container">
-        <div className="flex-box">
-          <i className="fas fa-shopping-cart"></i> {/* Buy Icon */}
-          <h3>Buy</h3>
-          <p>See what other slugs are selling.</p>
-        </div>
-        <div className="flex-box">
-          <i className="fas fa-dollar-sign"></i> {/* Sell Icon */}
-          <h3>Sell</h3>
-          <p>Sell your unused goods to fellow Slugs.</p>
-        </div>
-        <div className="flex-box">
-          <i className="fas fa-exchange-alt"></i> {/* Trade Icon */}
-          <h3>Trade</h3>
-          <p>Exchange items with slugs and get what you need.</p>
+        <div className="fb-logo">
+          <h1>slugmart</h1>
+          <p>Buy, sell, trade with slugs on Slugmart.</p>
         </div>
       </div>
-      <div className='landing-content'>
-        <h1>
-            Welcome to Slug Mart!
-        </h1>
-        <button onClick={handleGoogleLogin} className='banana-slug-button'>Sign Up / Sign In</button>
-        <p className='ucsc-warning'>
-            must have a @ucsc.edu email to sign up/in
-        </p>
+      <div className="landing-content">
+        <button onClick={handleGoogleLogin} className="banana-slug-button">
+          sign up / sign in
+        </button>
+        <p className="ucsc-warning">must have a @ucsc.edu email</p>
       </div>
-      </div>
-    
+    </div>
   );
 }
 
