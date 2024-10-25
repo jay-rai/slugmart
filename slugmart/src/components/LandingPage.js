0b73c46439494c11fc77f62bde86eedb7df4e869
Ethan Wong <ewong29@ucsc.edu>
2024-10-24 18:58:17 -0700

    disabled scrolling for landing page

--- slugmart/src/components/LandingPage.js
+++ slugmart/src/components/LandingPage.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LandingPage.css";
-import { useEffect, useState } from "react";
 import {
   GoogleAuthProvider,
   signInWithPopup,
@@ -15,18 +14,13 @@ function LandingPage() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
-  // handling login
   const handleGoogleLogin = async () => {
     const authProvider = new GoogleAuthProvider();
     try {
       const result = await signInWithPopup(auth, authProvider);
-      const credentials = GoogleAuthProvider.credentialFromResult(result);
       const user = result.user;
-
-      // checking if user apart of ucsc
       if (user.email.endsWith("@ucsc.edu")) {
-        console.log("User from ucsc", user);
-        addUserToDatabase(user); // add user to databse if they are ucsc user
+        addUserToDatabase(user);
         setUser(user);
       } else {
         console.log("User not from ucsc");
@@ -36,7 +30,6 @@ function LandingPage() {
     }
   };
 
-  // adding users to database
   const addUserToDatabase = async (user) => {
     try {
       const userRef = doc(db, "users", user.uid);
@@ -53,15 +46,22 @@ function LandingPage() {
   useEffect(() => {
     const listener = onAuthStateChanged(auth, (user) => {
       if (user && user.email.endsWith("@ucsc.edu")) {
-        console.log("User already logged in:", user);
         setUser(user);
         navigate("/account");
       } else {
         setUser(null);
       }
     });
-    return () => listener();
-  }, []);
+
+    // Disable scrolling for landing page
+    document.body.classList.add("no-scroll");
+
+    return () => {
+      // Enable scrolling again when component is unmounted
+      document.body.classList.remove("no-scroll");
+      listener();
+    };
+  }, [navigate]);
 
   return (
     <div className="landing-container">
