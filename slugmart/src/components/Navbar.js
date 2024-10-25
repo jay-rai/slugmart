e6934cde23c18f8e8a23eca941ce3ca197f99846
Ethan Wong <ewong29@ucsc.edu>
2024-10-24 20:07:32 -0700

    navbar frontend complete

--- slugmart/src/components/Navbar.js
+++ slugmart/src/components/Navbar.js
@@ -1,39 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './Navbar.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./Navbar.css";
+import logo from "../images/slugmartLogo.png";
 
 function Navbar({ handleLogout }) {
-    
-    return (
-        <nav className="navbar">
-            <ul className="nav-links">
-                <li>
-                    <NavLink to="/browse" className="nav-item">
-                        Browse
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/add-listing" className="nav-item">
-                        Sell
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/account" className="nav-item">
-                        <i className="fas fa-user"></i>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/settings" className="nav-item">
-                        <i className="fas fa-gears"></i>
-                    </NavLink>
-                </li>
-                <li>
-                    <button className="nav-item logout-btn" onClick={handleLogout}>
-                        <i className="fas fa-right-to-bracket"></i>
-                    </button>
-                </li>
-            </ul>
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="Slugmart Logo" />
+      </div>
+      <div className="slugmart">slugmart</div>
+      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+        <li className="nav-item">
+          <NavLink to="/browse">Browse</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/add-listing">Sell</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/account">
+            <i className="fas fa-user"></i>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/settings">
+            <i className="fas fa-gears"></i>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <button className="logout-btn" onClick={handleLogout}>
+            <i className="fas fa-right-to-bracket"></i>
+          </button>
+        </li>
+      </ul>
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsMobile(!isMobile)}
+      >
+        {isMobile ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"></i>
+        )}
+      </button>
     </nav>
   );
 }
