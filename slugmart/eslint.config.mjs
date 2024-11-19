f71634b1f804de56de520eae38460344411278b5
hgidha <hgidha@ucsc.edu>
2024-11-18 20:18:34 -0800

    Added Linting

---
+++ slugmart/eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      react: pluginReact
+    },
+    rules: {
+      // Disable react/react-in-jsx-scope rule
+      "react/react-in-jsx-scope": "off",
+      
+      // Disable react/prop-types rule
+      "react/prop-types": "off"
+    }
+  },
+  // Spread the recommended React rules but exclude the ones we've disabled
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    }
+  }
+];
